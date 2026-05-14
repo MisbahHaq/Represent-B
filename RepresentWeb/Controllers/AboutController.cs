f08cb475bdaa1853e2d40699c61c7a95a2460e7b
Misbah Haque <misbahmuneer2003@gmail.com>
2026-05-14 20:03:19 +0500

    Updated the Navbar & Footer

---
+++ RepresentWeb/Controllers/AboutController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class AboutController : Controller
+{
+    public IActionResult About()
+    {
+        return View();
+    }
+}
--- RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
+++ RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609b4524d7dcf850f3eef2029191144ad8dd436c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c6c48816d8b487ce65382df07cd7ed59811419")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

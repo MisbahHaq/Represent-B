fd19808e4e0a08ded85fa19d32be90ec632fbd4e
Misbah Haque <misbahmuneer2003@gmail.com>
2026-06-09 11:16:32 +0500

    Update

--- RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
+++ RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2930faba4ef9b9859d220bbba7c27bb265ab4084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c954937393a60bd1cb924af1eac7f04b8544837")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

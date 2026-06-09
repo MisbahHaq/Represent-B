db41a8f5f68061a2627742e005e926230a38c015
Misbah Haque <misbahmuneer2003@gmail.com>
2026-06-09 10:09:44 +0500

    Point navbar and bookmark link to Home
    Update the bookmark view and layout so the 'Start browsing' link and navbar brand route to the Home controller (asp-controller="Home", asp-action="Index"). The rest of the diff is rebuilt artifacts and generated obj/bin files (assembly info, static web assets, sourcelink, caches) that changed due to a rebuild and project path casing updates.

--- RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
+++ RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae46c82f6d43322b0a65afd8a12682df864cfaca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c558a5e0a4be50422bdd8c8ea98410cbb4021046")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

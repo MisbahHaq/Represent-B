09823991d4afc3757281a9e329d2a951350e6892
Misbah Haque <misbahmuneer2003@gmail.com>
2026-06-15 09:44:01 +0500

    Refactor bookmark button state handling
    Centralize and stabilize bookmark UI/state logic on product pages: add a data-bookmarked attribute and a setBookmarkButtonState helper to toggle color, icon class and is-bookmarked state; update toggleBookmark to use the dataset and a consistent nextBookmarkedState; store bookmark IDs as strings in localStorage and normalize reads/writes. Also stop changing the global bookmark icon color in updateBookmarkCount and apply setBookmarkButtonState when initializing the product button. Minor formatting tweaks and build/obj artifacts updated from a rebuild.

--- RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
+++ RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d810def4dd3b2e3047bb75ed5c075cf336f53307")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87572f8335e97fedbefddbf9f3117cc57213f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

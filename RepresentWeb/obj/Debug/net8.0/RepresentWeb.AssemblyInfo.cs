f4a699bdb15e630eda74fed6e9a3fda9c04bdd5a
Misbah Haque <misbahmuneer2003@gmail.com>
2026-06-23 10:24:20 +0500

    Update build outputs and assembly info
    Regenerated build outputs and intermediary files. AssemblyInformationalVersion and SourceLink were updated from commit a4c41405... to c7ab9ef9..., causing refreshed binaries (bin/) and obj/ artifacts (dll, exe, pdb, apphost, ref).

--- RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
+++ RepresentWeb/obj/Debug/net8.0/RepresentWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c41405f8608c6ceeb72bdfe856f6a2c5e88eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ab9ef932df5531a7785366ff2a9770b26f734f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepresentWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
